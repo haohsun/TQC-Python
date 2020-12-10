9800b4008576734458fff33596103f41410144c3
HaoHsunLi <li880809@gmail.com>
2020-12-10 08:00:33 +0800

    102 浮點數格式化輸出
    1. 題目說明:
    請開啟PYD102.py檔案，依下列題意進行作答，輸入浮點數及進行格式化輸出，使輸出值符合題意要求。作答完成請另存新檔為PYA102.py再進行評分。
    
    2. 設計說明：
    請撰寫一程式，輸入四個分別含有小數1到4位的浮點數，然後將這四個浮點數以欄寬為7、欄與欄間隔一個空白字元、每列印兩個的方式，先列印向右靠齊，再列印向左靠齊，左右皆以直線 |（Vertical bar）作為邊界。
    
    提示：輸出浮點數到小數點後第二位。
    
    3. 輸入輸出：
    輸入說明
    四個浮點數
    
    輸出說明
    格式化輸出
    
    輸入輸出範例
    
    範例輸入
    
    23.12
    395.3
    100.4617
    564.329
    
    範例輸出
    
    |  23.12  395.30|
    | 100.46  564.33|
    |23.12   395.30 |
    |100.46  564.33 |

---
+++ PYD102.py
@@ -0,0 +1,15 @@
+# TODO
+a = eval(input())
+b = eval(input())
+c = eval(input())
+d = eval(input())
+
+# 靠右對齊
+# TODO
+print("|{:>7.2f} {:>7.2f}|".format(a,b))
+print("|{:>7.2f} {:>7.2f}|".format(c,d))
+
+# 靠左對齊
+# TODO
+print("|{:<7.2f} {:<7.2f}|".format(a,b))
+print("|{:<7.2f} {:<7.2f}|".format(c,d))
