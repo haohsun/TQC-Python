50e604bdc819203fc1f0164069caac4e5396594f
HaoHsunLi <li880809@gmail.com>
2020-12-14 12:32:08 +0800

    107 數值計算
    1. 題目說明:
    請開啟PYD107.py檔案，依下列題意進行作答，計算五個數字之數值、總和及平均數，使輸出值符合題意要求。作答完成請另存新檔為PYA107.py再進行評分。
    
    2. 設計說明：
    請撰寫一程式，讓使用者輸入五個數字，計算並輸出這五個數字之數值、總和及平均數。
    
    提示：總和與平均數皆輸出到小數點後第1位。
    
    3. 輸入輸出：
    輸入說明
    五個數字
    
    輸出說明
    輸出五個數字
    總和
    平均數
    
    輸入輸出範例
    
    範例輸入1
    
    20
    40
    60
    80
    100
    
    範例輸出1
    
    20 40 60 80 100
    Sum = 300.0
    Average = 60.0
    
    範例輸入2
    
    88.7
    12
    56
    132.55
    3
    
    範例輸出2
    
    88.7 12 56 132.55 3
    Sum = 292.2
    Average = 58.5

---
+++ PYD107.py
@@ -0,0 +1,15 @@
+num1 = eval(input())
+num2 = eval(input())
+num3 = eval(input())
+num4 = eval(input())
+num5 = eval(input())
+
+# TODO
+num_sum = num1 + num2 + num3 + num4 + num5
+average = num_sum / 5
+
+print(num1, num2, num3, num4, num5)
+
+
+print("Sum = {:.1f}".format(num_sum)) 
+print("Average = {:.1f}".format(average))
