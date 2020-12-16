281f89bfe9254b7caf06d705e53ea43569c30ef3
HaoHsunLi <li880809@gmail.com>
2020-12-17 07:52:54 +0800

    206 等級判斷
    1. 題目說明:
    請開啟PYD206.py檔案，依下列題意進行作答，判斷輸入值所對應的等級，使輸出值符合題意要求。作答完成請另存新檔為PYA206.py再進行評分。
    
    2. 設計說明：
    請使用選擇敘述撰寫一程式，根據使用者輸入的分數顯示對應的等級。標準如下表所示：
    
    分　數	等級
    80 ~ 100	A
    70 ~ 79	B
    60 ~ 69	C
    <= 59	F
    3. 輸入輸出：
    輸入說明
    一個整數
    
    輸出說明
    判斷輸入值所對應的等級
    
    輸入輸出範例
    
    範例輸入
    
    79
    
    範例輸出
    
    B

---
+++ PYD206.py
@@ -0,0 +1,10 @@
+number = eval(input())
+
+if number <= 100 and number >= 80:
+    print("A")
+elif number <= 79 and number >= 70:
+    print("B")
+elif number <= 69 and number >= 60:
+    print("C")
+elif number <= 59:
+    print("F")
