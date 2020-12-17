b49fc84bdbf5940be3e8f9ba7b8cde7e248f77dd
HaoHsunLi <li880809@gmail.com>
2020-12-17 08:14:04 +0800

    208 十進位換算
    1. 題目說明:
    請開啟PYD208.py檔案，依下列題意進行作答，依輸入值進行進位轉換，使輸出值符合題意要求。作答完成請另存新檔為PYA208.py再進行評分。
    
    2. 設計說明：
    請使用選擇敘述撰寫一程式，讓使用者輸入一個十進位整數num(0 ≤ num ≤ 15)，將num轉換成十六進位值。
    
    提示：轉換規則 = 十進位0~9的十六進位值為其本身，十進位10~15的十六進位值為A~F。
    
    3. 輸入輸出：
    輸入說明
    一個數值
    
    輸出說明
    將此數值轉換成十六進位值
    
    輸入輸出範例
    
    範例輸入1
    
    13
    
    範例輸出1
    
    D
    
    範例輸入2
    
    8
    
    範例輸出2
    
    8

---
+++ PYD208.py
@@ -0,0 +1,16 @@
+n = eval(input())
+
+if n <= 9:
+    print(n)
+elif n % 9 == 1:
+    print("A")
+elif n % 9 == 2:
+    print("B")
+elif n % 9 == 3:
+    print("C")
+elif n % 9 == 4:
+    print("D")
+elif n % 9 == 5:
+    print("E")
+elif n % 9 == 6:
+    print("F")
