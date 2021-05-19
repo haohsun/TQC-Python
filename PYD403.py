4384339b402d8c54778fb67028c108fced4bac34
HaoHsunLi <li880809@gmail.com>
2021-05-19 10:23:30 +0800

    403 倍數總和計算
    1. 題目說明:
    請開啟PYD403.py檔案，依下列題意進行作答，使輸出值符合題意要求。作答完成請另存新檔為PYA403.py再進行評分。
    
    2. 設計說明：
    請撰寫一程式，讓使用者輸入兩個正整數a、b（a<=b），輸出從a到b（包含a和b）之間4或9的倍數（一列輸出十個數字、欄寬為4、靠左對齊）以及倍數之個數、總和。
    
    3. 輸入輸出：
    輸入說明
    
    兩個正整數a、b（a<=b）
    
    輸出說明
    
    格式化輸出兩個正整數之間4或9之倍數（包含a和b）
    倍數個數
    倍數總合
    
    輸入輸出範例
    範例輸入1
    
    5
    55
    範例輸出1
    
    8   9   12  16  18  20  24  27  28  32
    36  40  44  45  48  52  54
    17
    513
    
    範例輸入2
    
    4
    9
    範例輸出2
    
    4   8   9
    3
    21

---
+++ PYD403.py
@@ -0,0 +1,15 @@
+a = eval(input())
+b = eval(input())
+count = 0
+total = 0
+
+for i in range(a, b+1):
+    if i % 4 == 0 or i % 9 == 0:
+        print("{:<4d}".format(i),end = "")
+        total += i
+        count += 1
+        if count % 10 == 0:
+            print()
+print()
+print(count)
+print(total)
